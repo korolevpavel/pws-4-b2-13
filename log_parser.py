90ff17988982d17ed39086f7a8c0e0846fc72b44
Korolev Pavel <korolev.vrn@gmail.com>
2019-10-11 22:51:51 +0300

    Добавлен анализ количества запросов

--- log_parser.py
+++ log_parser.py
@@ -19,6 +19,19 @@ def checkIp(filename, ip):
 
     return counter
 
+def requestVal(filename):
+    fp =open(filename, encoding='UTF-8')
+    text_lines = fp.readlines()
+    fp.close()
+
+    counter = collections.Counter()
+
+    for line in text_lines:
+        words = line.split()
+        counter[words[0]] += 1
+
+    return counter
+
 if __name__ == '__main__':
    
     counter = checkIp(sys.argv[1], "79.136.245.135")
@@ -28,3 +41,13 @@ if __name__ == '__main__':
     counter = checkIp(sys.argv[1], "127.0.0.1")
     for ip in counter:
         print("Запросов с {} сделано -- {}".format(ip, counter[ip]))
+
+    counter = requestVal(sys.argv[1])
+    n = 2
+    for word, cnt in counter.most_common(1):
+        print("Максимальное число запросов с {} -- {}".format(word, cnt))
+
+    counter = requestVal(sys.argv[1])
+    n = 2
+    for word, cnt in counter.most_common()[:-n-1:-1]:
+        print("Минимальное число запросов с {} -- {}".format(word, cnt))
