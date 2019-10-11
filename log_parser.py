cb379565e8be8d95641f9ffb868350fdb7091513
Korolev Pavel <korolev.vrn@gmail.com>
2019-10-12 01:05:14 +0300

    Вывод среднего значения + рефакторинг

--- log_parser.py
+++ log_parser.py
@@ -2,10 +2,15 @@ import sys
 import collections
 
 
-def checkIp(filename, ip):
+def getTextLines(filename):
     fp = open(filename, encoding='UTF-8')
     textLines = fp.readlines()
     fp.close()
+
+    return textLines
+
+
+def checkIp(textLines, ip):
 
     counter = collections.Counter()
 
@@ -20,37 +25,51 @@ def checkIp(filename, ip):
     return counter
 
 
-def requestVal(filename):
-    fp = open(filename, encoding='UTF-8')
-    text_lines = fp.readlines()
-    fp.close()
+def requestVal(textLines):
 
     counter = collections.Counter()
 
-    for line in text_lines:
+    for line in textLines:
         words = line.split()
         counter[words[0]] += 1
 
     return counter
 
 
+def meanVal(textLines):
+
+    ipSet = set()
+    mean = 0
+    for line in textLines:
+        words = line.split()
+        ipSet.add(words[0])
+
+    if len(ipSet) != 0:
+        mean = round(sum(1 for _ in textLines) / len(ipSet))
+
+    return mean
+
+
 if __name__ == '__main__':
 
-    filename = sys.argv[1]
-    counter = checkIp(filename, "79.136.245.135")
+    textLines = getTextLines(sys.argv[1])
+
+    counter = checkIp(textLines, "79.136.245.135")
     for ip in counter:
         print("Запросов с {} сделано -- {}".format(ip, counter[ip]))
 
-    counter = checkIp(filename, "127.0.0.1")
+    counter = checkIp(textLines, "127.0.0.1")
     for ip in counter:
         print("Запросов с {} сделано -- {}".format(ip, counter[ip]))
 
-        counter = requestVal(filename)
-        n = 2
-        for word, cnt in counter.most_common(1):
-            print("Максимальное число запросов с {} -- {}".format(word, cnt))
+    counter = requestVal(textLines)
+    for word, cnt in counter.most_common(1):
+        print("Максимальное число запросов с {} -- {}".format(word, cnt))
 
-        counter = requestVal(filename)
-        n = 2
-        for word, cnt in counter.most_common()[:-n-1:-1]:
-            print("Минимальное число запросов с {} -- {}".format(word, cnt))
+    counter = requestVal(textLines)
+    n = 2
+    for word, cnt in counter.most_common()[:-n-1:-1]:
+        print("Минимальное число запросов с {} -- {}".format(word, cnt))
+
+    meanRequest = meanVal(textLines)
+    print("Среднее число запросов -- {}".format(meanRequest))
